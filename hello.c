74a02754bd52a6af32ffd7ff31412a64b122b747
Pundits-iTech <mohammedaalli088@gmail.com>
2022-06-29 18:46:24 +0000

    This my first hello program but not from alx

--- hello.c
+++ hello.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - this is a fuction to print hello world
+ * Return: Always 0 (success)
+ */
+
+
 int main(void)
-{ 
+{
+
 	printf("Hello world");
-	return 0;
+	return (0);
 }
