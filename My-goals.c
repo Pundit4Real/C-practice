8f085ae46259aa12d93255fda4480fb4a67944f1
Pundits-iTech <mohammedaalli088@gmail.com>
2022-06-29 14:07:56 +0000

    my goals and interest as a software engineer

---
+++ My-goals.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("My goals for learning and constantely practicing coding is to become \n a full stack software engineer with a main specification in Artificial intelligence(AI)");
+	printf("programming languages of focus are;");
+	printf("C language \n python \n c++ Java");
+	
+	return 0;	
+
+}
